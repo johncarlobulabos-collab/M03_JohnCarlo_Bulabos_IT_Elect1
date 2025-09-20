8c10cfe550a92a4151799e3d79f6b98c2b0059ee
johncarlobulabos-collab <johncarlobulabos@gmail.com>
2025-09-20 21:50:43 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>JAY MARVIN CAMPOSO</Text>
+      <Text>John Carlo Bulabos</Text>
       <StatusBar style="auto" />
     </View>
   );
